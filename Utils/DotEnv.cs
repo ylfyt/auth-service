bf4814c3457247b5fa29b74f811ec085b5059bb9
ylfyt <ayudi3521@gmail.com>
2022-07-25 13:12:17 +0700

    Initial auth service using asp.net

---
+++ Utils/DotEnv.cs
@@ -0,0 +1,23 @@
+namespace auth_sevice.Src.Utils
+{
+  public static class DotEnv
+  {
+    private const string ENV_FILENAME = ".env";
+    public static void Init()
+    {
+      string filePath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), ENV_FILENAME);
+      if (!File.Exists(filePath))
+        return;
+
+      foreach (var line in File.ReadAllLines(filePath))
+      {
+        var parts = line.Split(new[] { '=' }, 2);
+
+        if (parts.Length != 2)
+          continue;
+
+        Environment.SetEnvironmentVariable(parts[0], parts[1]);
+      }
+    }
+  }
+}
---
+++ Utils/ServerInfo.cs
@@ -0,0 +1,34 @@
+namespace auth_sevice.Src.Utils
+{
+  public static class ServerInfo
+  {
+    public static string DB_CONNECT { get; set; } = string.Empty;
+    public static string JWT_ACCESS_TOKEN_SECRET_KEY { get; set; } = string.Empty;
+    public static string JWT_REFRESH_TOKEN_SECRET_KEY { get; set; } = string.Empty;
+    public static double JWT_ACCESS_TOKEN_EXPIRY_TIME { get; set; }
+    public static double JWT_REFRESH_TOKEN_EXPIRY_TIME { get; set; }
+    public static void Init()
+    {
+      var dbConnect = Environment.GetEnvironmentVariable("DB_CONNECT");
+      var accessTokenSecretKey = Environment.GetEnvironmentVariable("JWT_ACCESS_TOKEN_SECRET_KEY");
+      var refreshTokenSecretKey = Environment.GetEnvironmentVariable("JWT_REFRESH_TOKEN_SECRET_KEY");
+      var accessTokenExpiryTime = Environment.GetEnvironmentVariable("JWT_ACCESS_TOKEN_EXPIRY_TIME");
+      var refreshTokenExpiryTime = Environment.GetEnvironmentVariable("JWT_REFRESH_TOKEN_EXPIRY_TIME");
+
+      if (
+        dbConnect == null ||
+        accessTokenSecretKey == null ||
+        refreshTokenSecretKey == null ||
+        accessTokenExpiryTime == null ||
+        refreshTokenExpiryTime == null
+      )
+        throw new Exception("======= SOME ENV VARIABLE IS NULL =======");
+
+      ServerInfo.DB_CONNECT = dbConnect;
+      ServerInfo.JWT_ACCESS_TOKEN_SECRET_KEY = accessTokenSecretKey;
+      ServerInfo.JWT_REFRESH_TOKEN_SECRET_KEY = refreshTokenSecretKey;
+      ServerInfo.JWT_ACCESS_TOKEN_EXPIRY_TIME = Double.Parse(accessTokenExpiryTime);
+      ServerInfo.JWT_REFRESH_TOKEN_EXPIRY_TIME = Double.Parse(refreshTokenExpiryTime);
+    }
+  }
+}
